e821c5ec5780072f4d7955c118acef7c115636d2
ce-a11y <110133088+ce-a11y@users.noreply.github.com>
2022-08-07 04:22:27 -0300

    Add files via upload

---
+++ album/teste.php
@@ -0,0 +1,18 @@
+<?php
+
+$fotos = [
+    '../upload/1.jpg',
+    '../upload/2.jpg',
+    '../upload/3.jpg',
+    '../upload/4.jpg',
+    '../upload/5.jpg',
+    '../upload/6.jpg',
+    '../upload/7.jpg',
+    '../upload/8.jpg',
+    '../upload/9.jpg',
+]
+
+
+
+
+?>
